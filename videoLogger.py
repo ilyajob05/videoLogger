11ca541e13c0e34f9016e7a4deb7123b732b6cc8
Ilya Derendyaev <IVDerendyaev@sberbank.ru>
2021-02-24 00:41:14 +0300

    update for python3

--- videoLogger.py
+++ videoLogger.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import time
 import cv2
 
@@ -8,13 +6,13 @@ cap = cv2.VideoCapture(0)
 # get resolution
 ret, frame = cap.read()
 if ret != True:
-    print 'not frame from camera'
+    print('not frame from camera')
     exit(0)
 h,w,c = frame.shape
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-out = cv2.VideoWriter('./output_' + time.asctime() + '.avi', fourcc, 20.0, (w, h))
+out = cv2.VideoWriter('./output_' + time.asctime() + '.avi', fourcc, 30.0, (w, h))
 
 while(True):
     # Capture frame-by-frame
